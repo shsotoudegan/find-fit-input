3dacecd63027e61fd3fc8b9f2b859bbbca64003b
shsotoudegan <shsotoudegan@gmail.com>
2023-09-08 17:05:02 +0330

    set kwargs for func arguments

--- example_function.py
+++ example_function.py
@@ -1,10 +1,18 @@
 from find_fit_input import finder
+import sys
 
-@finder
-def example_function(a):
+
+def step(a, b):
+    return {"a": a + 1, "b": b}
+
+@finder(step=step)
+def example_function(a, b):
     result = 0
     for i in range(a + 1):
         result += i ** i
-    return result
+    return result + b
 
-print(example_function(3))
+
+sys.set_int_max_str_digits(1_000_000_000)
+
+print(example_function(a=3, b=1))
--- find_fit_input.py
+++ find_fit_input.py
@@ -6,19 +6,23 @@ from functools import wraps
 from timeit import timeit
 
 
-def finder(func, target=0.04, error=0.01, n=2):
+def finder(func=None, *, step, target=0.04, error=0.01, n=2):
     """A wrapper, find input, which in (target time - error, target time + error) interval"""
-    @wraps(func)
-    def wrapper(a):
-        period = 0
-        inp = a
-        a = -1
-        while not ((period <= (target + error)) and (period >= (target - error))):
-            a += 1
-            period = timeit(stmt="function(a)", number=n, globals={'a': a, 'function': func})/float(n)
-            if period > (target + error):
-                print("NOT EXIST!")
-        else:
-            print(f"function: {func.__name__},\ninput: {a},\ntime: {period},\ntarget time: {target},\nerror: {error},\ndifference: {target - period}")
-        return func(inp)
-    return wrapper
+    def finder_decorator(func):
+        @wraps(func)
+        def wrapper(**kwargs):
+            period = 0
+            while not ((period <= (target + error)) and (period >= (target - error))):
+                kwargs = step(**kwargs)
+                period = timeit(stmt="function(**kwargs)", number=n, globals={'kwargs': kwargs, 'function': func})/float(n)
+                if period > (target + error):
+                    print("NOT EXIST!")
+            else:
+                print(f"function: {func.__name__},\ninput: {kwargs},\ntime: {period},\ntarget time: {target},\nerror: {error},\ndifference: {target - period}")
+            return func(**kwargs)
+        return wrapper
+    
+    if func is None:
+        return finder_decorator
+    else:
+        return finder_decorator(func)
